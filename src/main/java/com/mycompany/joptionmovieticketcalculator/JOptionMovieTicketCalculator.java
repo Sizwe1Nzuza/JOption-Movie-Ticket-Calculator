3f2381b6865532f67d4aa4968bdebaf466705600
Sizwe1Nzuza <St10483555@vcconnect.edu.za>
2025-03-15 18:51:47 +0200

    Update JOptionMovieTicketCalculator.java

--- src/main/java/com/mycompany/joptionmovieticketcalculator/JOptionMovieTicketCalculator.java
+++ src/main/java/com/mycompany/joptionmovieticketcalculator/JOptionMovieTicketCalculator.java
@@ -16,10 +16,16 @@ public class JOptionMovieTicketCalculator {
        String Input01 = JOptionPane.showInputDialog(null, "How Old Are You? ") ;
       
       int age = Integer.parseInt(Input01) ;
-      if (age <=12) {
+        if (age <=5 {
+        JOptionPane.showMessageDialog(null, "Your ticket is free") ;
+        }
+            
+     else if (age <=12) {
       JOptionPane.showMessageDialog(null, "The Ticket will cost: R5") ;
+         
       } else if  (age <60) {
               JOptionPane.showMessageDialog (null, "The Ticket will cost: R10") ;
+         
       } else if (age >=60) {
       JOptionPane.showMessageDialog(null, "The Ticket will cost: R7 ");
               }
